5edc47cdcc1a04d2468db51f88a4c5e18091f1bf
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2023-03-28 18:01:56 +0500

    face status detector

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from .AI.imagga.barcodes import barcode_runner
 from .AI.imagga.colors import colors_runner
+from .AI.imagga.faces import face_detection_runner
 from .AI.imagga.tags import ImaggaTagsEndpoint, tags_runner
 from config import load_config, BASE_DIR
 
@@ -10,7 +11,8 @@ def run():
     command = int(input('Введите команду:\n'
                         '1: Получить самый вероятный объект с картинки\n'
                         '2: Получить значение штрих-кода\n'
-                        '3: Получить цвета на картинке\nВвод: '))
+                        '3: Получить цвета на картинке\n'
+                        '4: Проверить есть ли там морда!\nВвод: '))
     if command == 1:
         tags_runner(
             config.IMAGGA_API_KEY,
@@ -25,6 +27,12 @@ def run():
         )
     elif command == 3:
         colors_runner(
+            config.IMAGGA_API_KEY,
+            config.IMAGGA_API_SECRET,
+            image_url
+        )
+    elif command == 4:
+        face_detection_runner(
             config.IMAGGA_API_KEY,
             config.IMAGGA_API_SECRET,
             image_url
